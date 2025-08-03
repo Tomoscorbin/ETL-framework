d681933a90c5f30f52b5b8fcb948848e709c76a8
Tom Corbin <tom_corbin1@hotmail.com>
2025-08-03 17:35:15 +0100

    docs conf update

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,11 @@ extensions: list[str] = [
     "myst_parser",
 ]
 
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 

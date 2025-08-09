9fcfc7fd2df03244c50a0b5e0c84c3add2f5b7df
Tom Corbin <tom_corbin1@hotmail.com>
2025-08-09 19:04:59 +0100

    import fix

--- src/delta_engine/execute/foreign_key_executor.py
+++ src/delta_engine/execute/foreign_key_executor.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from src.logger import LOGGER
-from src.delta_engine.actions import AddForeignKey, DropForeignKey
+from src.delta_engine.actions import ForeignKeyAdd, ForeignKeyDrop
 from src.delta_engine.execute.renderer import SqlRenderer, construct_full_name
 
 class ForeignKeyExecutor:
@@ -8,12 +8,12 @@ class ForeignKeyExecutor:
         self.spark = spark
         self.renderer = SqlRenderer()
 
-    def drop(self, action: DropForeignKey) -> None:
+    def drop(self, action: ForeignKeyDrop) -> None:
         full = construct_full_name(action.catalog_name, action.schema_name, action.table_name)
         LOGGER.info("Dropping FK %s on %s", action.constraint_name, full)
         self.spark.sql(self.renderer.drop_foreign_key(full, action.constraint_name))
 
-    def add(self, action: AddForeignKey) -> None:
+    def add(self, action: ForeignKeyAdd) -> None:
         src_full = construct_full_name(action.catalog_name, action.schema_name, action.source_table_name)
         ref_full = construct_full_name(action.catalog_name, action.schema_name, action.reference_table_name)
         LOGGER.info("Adding FK %s on %s -> %s", action.constraint_name, src_full, ref_full)

657b90bc42e1a0051214c43e92625236e5b3ccd6
Tomos Corbin <tom_corbin1@hotmail.com>
2025-08-10 07:43:22 +0100

    rules class var

--- src/delta_engine/compile/plan_validator.py
+++ src/delta_engine/compile/plan_validator.py
@@ -5,13 +5,9 @@ from .validation_rules import ValidationRule, NoAddNotNullColumnsRule
 
 @dataclass
 class PlanValidator:
-    rules: list[ValidationRule]
-
-    @classmethod
-    def with_default_rules(cls) -> Self:
-        return cls(rules=[
-            NoAddNotNullColumnsRule(),
-        ])
+    rules: ClassVar[list[ValidationRule]] = [
+        NoAddNotNullColumnsRule,
+    ]
 
     def validate(self, plan: Plan) -> None:
         for rule in self.rules:
